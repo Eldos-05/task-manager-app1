622637517b253e6d186c5c6e371966c2d88f1989
Eldos-05 <eldos.220505@gmail.com>
2024-12-12 15:06:44 +0600

    Set up React app with UI

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTask = () => {
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Task Manager</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Enter a task"
+      />
+      <button onClick={addTask}>Add Task</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
